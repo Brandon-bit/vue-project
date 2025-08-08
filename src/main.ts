609da26ddcc14477382bf535efc2dce008f37323
Gerardo C <gerardo.carrizosa@dcvent.com>
2025-08-08 12:43:52 -0600

    fix: Remove unnecessary lines

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import './style.css'
 import App from './App.vue'
 // Router
 import router from './router'
-<<<<<<< Updated upstream
 // Pinia
 import { createPinia } from 'pinia'
 // Toastification
@@ -16,19 +15,12 @@ import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 // Theme
 import { useThemeStore } from './store/theme'
-=======
-import { createPinia } from 'pinia'
->>>>>>> Stashed changes
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-<<<<<<< Updated upstream
 app.use(Toast)
 app.use(FloatingVue)
 const themeStore = useThemeStore()
 themeStore.loadTheme()
-=======
-app.use(createPinia())
->>>>>>> Stashed changes
 app.mount('#app')
