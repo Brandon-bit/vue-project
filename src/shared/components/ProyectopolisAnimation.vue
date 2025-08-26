9fc2d35d984ea84a682e401bcf23917b37df56ea
Gerardo C <gerardo.carrizosa@dcvent.com>
2025-08-26 10:18:46 -0600

    Add branding loader for app initialization

---
+++ src/shared/components/ProyectopolisAnimation.vue
@@ -0,0 +1,32 @@
+<template>
+    <div ref="container" class="lottie-container"></div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import lottie from 'lottie-web'
+
+const container = ref(null)
+let animation = null
+
+onMounted(() => {
+    animation = lottie.loadAnimation({
+        container: container.value,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: '/icono.json'
+    })
+})
+
+onBeforeUnmount(() => {
+    if (animation) animation.destroy()
+})
+</script>
+
+<style scoped>
+.lottie-container {
+    width: 200px;
+    height: 200px;
+}
+</style>
