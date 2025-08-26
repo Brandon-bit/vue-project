26ec9cf6afaf256cd66e3a6bda207c47cf6ac4d7
Gerardo C <gerardo.carrizosa@dcvent.com>
2025-08-26 10:16:38 -0600

    Add skeleton loader for tables

---
+++ src/shared/components/BaseSkeletonTable.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div
+        class="rounded-box border border-base-content/5 bg-base-100 my-5 w-full max-w-[100vw] overflow-hidden"
+    >
+        <table class="table w-full">
+            <thead>
+                <tr>
+                    <th>
+                        <div class="skeleton h-4 w-10"></div>
+                    </th>
+                    <th>
+                        <div class="skeleton h-4 w-30"></div>
+                    </th>
+                    <th>
+                        <div class="skeleton h-4 w-30"></div>
+                    </th>
+                    <th>
+                        <div class="skeleton h-4 w-30"></div>
+                    </th>
+                    <th>
+                        <div class="skeleton h-4 w-20"></div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="n in 6" :key="n">
+                    <td>
+                        <div class="skeleton h-4 w-10"></div>
+                    </td>
+                    <td>
+                        <div class="skeleton h-4 w-30"></div>
+                    </td>
+                    <td>
+                        <div class="skeleton h-4 w-30"></div>
+                    </td>
+                    <td>
+                        <div class="skeleton h-4 w-30"></div>
+                    </td>
+                    <td>
+                        <div class="skeleton h-8 w-20 rounded-md"></div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
