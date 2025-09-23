2ea5523b4503e1e22079ef070086efbfcf5cfe58
Gerardo C <gerardo.carrizosa@dcvent.com>
2025-09-23 13:34:46 -0600

    Modify functions validations and add new table button

--- src/shared/validations/globalValidation.ts
+++ src/shared/validations/globalValidation.ts
@@ -1,6 +1,10 @@
 import { z } from 'zod'
 
-export const selectValidator = (message: string, requiredMessage : string = message) =>
+export const selectValidator = (
+    message: string,
+    requiredMessage: string = message,
+    allowZero: boolean = false
+) =>
     z.preprocess(
         (val) => {
             if (val === '' || val === null || val === undefined) return undefined
@@ -11,15 +15,28 @@ export const selectValidator = (message: string, requiredMessage : string = mess
                 invalid_type_error: requiredMessage,
                 required_error: requiredMessage
             })
-            .refine((val) => !isNaN(val), {
-                message
-            })
+            .refine(
+                (val) => {
+                    if (isNaN(val)) return false
+                    if (!allowZero && val === 0) return false
+                    return true
+                },
+                {
+                    message
+                }
+            )
     )
 
-export const numberValidator = (message: string, minMessage: string = message) => {
+export const numberValidator = (
+    message: string,
+    allowNegative: boolean = false,
+    minMessage?: string
+) => {
     return z.preprocess(
         (val) => (val === '' ? undefined : val),
-        z.number({ required_error: message }).min(1, minMessage)
+        allowNegative
+            ? z.number({ required_error: message })
+            : z.number({ required_error: message }).min(1, minMessage || message)
     )
 }
 
@@ -30,7 +47,6 @@ export const stringValidator = (message: string, minMessage: string, minLength: 
 export const dateValidator = (message: string) => {
     return z
         .string({ required_error: message })
-        .refine((val) => !val || /^\d{4}-\d{2}-\d{2}$/.test(val), {
-            message: message
-        })
+        .min(1, { message })
+        .regex(/^\d{4}-\d{2}-\d{2}$/, { message })
 }
