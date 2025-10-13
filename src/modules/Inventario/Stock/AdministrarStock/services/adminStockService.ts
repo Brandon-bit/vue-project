f5a7ae3039edf6ed6fb3c6664278ef802ad536e7
ErikJVG <erkjssvg@gmail.com>
2025-10-13 14:24:17 -0600

    actualizacion vista admin stock

---
+++ src/modules/Inventario/Stock/AdministrarStock/services/adminStockService.ts
@@ -0,0 +1,15 @@
+import axiosApiInstance from '@/api/axiosApiInstance'
+import type { ApiResponseType } from '@/shared/types/apiResponseType'
+import type { adminResponseType } from '@inventario/Stock/AdministrarStock/types/adminResponseType'
+import type { adminRequestType } from '@inventario/Stock/AdministrarStock/types/adminRequestType'
+import type { PagedResponseType } from '@/shared/types/pagedResponseType'
+
+export const getAdminStockService = async (page : number, pageSize: number) : Promise<ApiResponseType<PagedResponseType<adminResponseType>>> => {
+  const response = await axiosApiInstance.get('/Movimiento/kardex"', {
+    params: {
+      limit: page,
+      skip: pageSize
+    }
+  })
+  return response.data
+}
