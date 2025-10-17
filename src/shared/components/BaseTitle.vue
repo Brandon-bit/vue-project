407ac514e1f4aa717bb191eda9822b086c2feb49
Brandon Josue <brandonsalinas792@gmail.com>
2025-10-17 17:10:44 -0600

    refactor: replace custom title with BaseTitle component in AccountingPoliciesView

---
+++ src/shared/components/BaseTitle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        title: string
+        subtitle?: string
+    }>(),
+    {
+        title: 'Title'
+    }
+)
+</script>
+
+<template>
+    <div>
+        <h2 class="text-3xl !font-semibold">{{ props.title }}</h2>
+        <p v-if="props.subtitle" class="text-gray-500 mt-1">
+            {{ props.subtitle }}
+        </p>
+    </div>
+</template>
