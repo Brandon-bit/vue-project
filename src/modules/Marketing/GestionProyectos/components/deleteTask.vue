d41cb3cdb58b902827ecee1c9692e9f3f499e029
MDhz89 <mdjho89@gmail.com>
2025-10-17 16:34:15 -0600

    componentes(vista gestion dde proyectos)

---
+++ src/modules/Marketing/GestionProyectos/components/deleteTask.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useProjectStore } from '../store/projectStore';
+const projectStore = useProjectStore();
+</script>
+
+<template>
+  <p>
+    ¿Estás seguro que deseas eliminar la tarea
+    <span class="!font-semibold"> "{{ projectStore.selectedTask?.title }}" ? </span>
+  </p>
+</template>
